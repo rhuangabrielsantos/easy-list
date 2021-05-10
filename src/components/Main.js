41e05ef902c25b3fde7fa1d2592a3f6e32dc55cb
rhuangabrielsantos <rhuan.santos@db1.com.br>
2021-05-10 14:51:53 -0300

    :rocket: Added animations

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
-import { FaFlag, FaRegFlag } from 'react-icons/fa';
+import { useState } from 'react'
+import { FaFlag, FaRegFlag, FaRegTrashAlt } from 'react-icons/fa'
+import { motion } from 'framer-motion'
 
 export default function Main() {
   const [ todo, setTodo ] = useState([])
@@ -34,14 +35,21 @@ export default function Main() {
       newTodo = [...newTodo, item]
     })
 
-    console.log(newTodo)
-
     setTodo(newTodo)
   }
 
-  useEffect(() => {
-    setTodo(todo)
-  }, [todo])
+  const item = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: -70 },
+  }
+
+  function trashNote(indexToChanged) {
+    let newTodo = todo.filter((item, index) => {
+      return index !== indexToChanged
+    })
+
+    setTodo(newTodo)
+  }
 
   return (
     <main className="flex flex-col items-center justify-start mt-5 w-screen">
@@ -62,17 +70,28 @@ export default function Main() {
       </div>
 
 
-      <div className="flex flex-col items-center justify-start mt-5 h-80 max-h-80 w-screen overflow-auto">
+      <div className="flex flex-col items-center justify-start mt-5 h-96 max-h-96 w-4/12 overflow-y-auto">
         {todo.map((note, index) => (
-          <div 
-            className="flex items-center justify-start rounded-md m-2 h-auto p-3 w-10/12 md:max-w-2xl bg-vonCount cursor-pointer" 
-            onClick={() => checkNote(index)}
+          <motion.div 
+            className="flex items-center justify-start rounded-md m-2 h-auto p-3 w-11/12 bg-vonCount cursor-pointer" 
             key={index}
+            initial="hidden"
+            animate="visible"
+            variants={item}
+            transition={{ duration: 0.3 }}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
           >
-            {note.checked ? <FaFlag className="w-5 h-auto text-cullen" /> : <FaRegFlag className="w-5 h-auto text-cullen" /> }
-            
-            <h3 className={"text-cullen font-workSans text-xl ml-3 " + (note.checked ? 'line-through' : '')}>{note.description}</h3>
-          </div>
+            <div className="w-1/12" onClick={() => checkNote(index)}>
+              {note.checked ? <FaFlag className="w-5 h-5 text-cullen" /> : <FaRegFlag className="w-5 h-5 text-cullen" /> }
+            </div>
+            <div className="w-10/12" onClick={() => checkNote(index)}>
+              <h3 className={"break-all text-cullen font-workSans text-xl ml-3 " + (note.checked ? 'line-through' : '')}>{note.description}</h3>
+            </div>
+            <div className="w-1/12" onClick={() => trashNote(index)}>
+              <FaRegTrashAlt className="w-5 h-5 text-cullen" />
+            </div>
+          </motion.div>
         ))}
       </div>
     </main>
