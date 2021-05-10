475693b37bee7f0990296a1bc1198e4ae378a41a
rhuangabrielsantos <rhuan.santos@db1.com.br>
2021-05-10 15:15:42 -0300

    :rocket: Implemented save in localstorage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FaFlag, FaRegFlag, FaRegTrashAlt } from 'react-icons/fa'
 import { motion } from 'framer-motion'
 
 export default function Main() {
   const [ todo, setTodo ] = useState([])
   const [ note, setNote ] = useState('')
+
+  useEffect(() => {
+    setTodo(JSON.parse(localStorage.getItem('todo')))
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todo', JSON.stringify(todo))
+  }, [todo])
 
   function createNewTodo(e) {
     e.preventDefault()
@@ -70,7 +78,7 @@ export default function Main() {
       </div>
 
 
-      <div className="flex flex-col items-center justify-start mt-5 h-96 max-h-96 w-4/12 overflow-y-auto">
+      <div className="flex flex-col items-center justify-start mt-5 h-96 max-h-96 w-11/12 md:w-9/12 lg:w-4/12 overflow-y-auto">
         {todo.map((note, index) => (
           <motion.div 
             className="flex items-center justify-start rounded-md m-2 h-auto p-3 w-11/12 bg-vonCount cursor-pointer" 
@@ -82,13 +90,13 @@ export default function Main() {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            <div className="w-1/12" onClick={() => checkNote(index)}>
+            <div className="w-1/12 flex items-center justify-center" onClick={() => checkNote(index)}>
               {note.checked ? <FaFlag className="w-5 h-5 text-cullen" /> : <FaRegFlag className="w-5 h-5 text-cullen" /> }
             </div>
             <div className="w-10/12" onClick={() => checkNote(index)}>
               <h3 className={"break-all text-cullen font-workSans text-xl ml-3 " + (note.checked ? 'line-through' : '')}>{note.description}</h3>
             </div>
-            <div className="w-1/12" onClick={() => trashNote(index)}>
+            <div className="w-1/12 flex items-center justify-center" onClick={() => trashNote(index)}>
               <FaRegTrashAlt className="w-5 h-5 text-cullen" />
             </div>
           </motion.div>
