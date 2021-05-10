984799ffa9f9cd16eb8789f22985d7ad187f0fd1
rhuangabrielsantos <rhuan.santos@db1.com.br>
2021-05-10 15:22:54 -0300

    :rocket: Created PWA

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './Home';
+import { registerServiceWorker } from './serviceWorker'
+
+registerServiceWorker();
 
 ReactDOM.render(
   <React.StrictMode>
